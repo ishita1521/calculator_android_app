1deac88e5b04088d2924bb7f5337b840fe823715
vagabon-09 <rajeshbhadra62@gmail.com>
2022-10-14 02:08:50 +0530

    Converting temperature

--- app/src/main/java/com/example/calculator/TempConvertor.java
+++ app/src/main/java/com/example/calculator/TempConvertor.java
@@ -29,17 +29,17 @@ ActivityTempConvertorBinding binding;
     }
     @SuppressLint("SetTextI18n")
     public void temp_result(View view){
-        if (!binding.editTextNumberSigned.getText().toString().isEmpty()){
+        if (!binding.editTextNumberSigned.getText().toString().isEmpty() && binding.editTextNumberSigned2.getText().toString().isEmpty()){
             float c = Float.parseFloat(binding.editTextNumberSigned.getText().toString());
             float st = (float) (c* 1.8);
             float temp = (st)+ 32;
             binding.textView37.setText(binding.editTextNumberSigned.getText().toString()+" °C    =   "+ temp +" "+"°F");
-        }else if (!binding.editTextNumberSigned2.getText().toString().isEmpty()){
+        }else if (!binding.editTextNumberSigned2.getText().toString().isEmpty() && binding.editTextNumberSigned.getText().toString().isEmpty()){
             float f = Float.parseFloat(binding.editTextNumberSigned2.getText().toString());
             float f2 = (float) (f - 32);
             float temp = (float) ((f2) * 0.5555555555555556);
             binding.textView37.setText(binding.editTextNumberSigned2.getText().toString()+" °F    =   "+ temp +" "+"°C");
-        }else{
+        }else if (!binding.editTextNumberSigned.getText().toString().isEmpty() &&!binding.editTextNumberSigned2.getText().toString().isEmpty() ){
             float c = Float.parseFloat(binding.editTextNumberSigned.getText().toString());
             float st = (float) (c* 1.8);
             float temp1 = (st)+ 32;
@@ -48,6 +48,9 @@ ActivityTempConvertorBinding binding;
             float f2 = (float) (f - 32);
             float temp2 = (float) ((f2) * 0.5555555555555556);
 
+            String fahrenheit = binding.editTextNumberSigned2.getText().toString()+" °F    =   "+ temp2 +" "+"°C";
+            String celsius = binding.editTextNumberSigned.getText().toString()+" °C    =   "+ temp1 +" "+"°F" ;
+            binding.textView37.setText(fahrenheit + "\n\n" + celsius);
         }
     }
 }
